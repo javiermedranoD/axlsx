80aba780b9bdad58ee45e193bc11cf196a9e96fc
javiermedranoD <javierlmedrano@gmail.com>
2017-06-22 12:19:47 -0500

    Modificación del controlador Products y vista

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,12 @@
 class ProductsController < ApplicationController
 	def index
-    	@products = Product.order('created_at DESC')
-  	end
+  @products = Product.order('created_at DESC')
+  respond_to do |format|
+    format.html
+     format.xlsx {
+    	response.headers['Content-Disposition'] = 'attachment; filename="all_products.xlsx"'
+  		}
+  end
+end
 
 end
