8b1e314856318fe41861bf78a25d9e3eff44fd9b
javiermedranoD <javierlmedrano@gmail.com>
2017-06-22 11:58:31 -0500

    Cambios en el modelo product
    - Se corre la migración
    - Se instala la gema Faker para poblar la db
    - Se hacen los cambios en seeds.rb
    - Se configura la ruta en routes.db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+30.times do
+  Product.create({title: Faker::Commerce.product_name, price: Faker::Commerce.price})
+end
