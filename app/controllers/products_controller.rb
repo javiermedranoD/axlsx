4be853e852ba8997cd9f7e54919ffea9af971291
javiermedranoD <javierlmedrano@gmail.com>
2017-06-22 19:03:08 -0500

    Changes in View Products index

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,12 +1,17 @@
 class ProductsController < ApplicationController
 	def index
-  @products = Product.order('created_at DESC')
-  respond_to do |format|
-    format.html
-     format.xlsx {
-    	response.headers['Content-Disposition'] = 'attachment; filename="all_products.xlsx"'
-  		}
-  end
-end
+	@products_list = Product.all
+  	@products = Product.order('created_at DESC')
+	  respond_to do |format|
+	    format.html
+	     format.xlsx {
+	    	response.headers['Content-Disposition'] = 'attachment; filename="all_products.xlsx"'
+	  		}
+	  end
+	end
+
+	def show
+	  @product = Product.find(params[:id])
+	end
 
 end
